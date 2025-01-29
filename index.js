0b8874c8f158aaf1d55ff32fe40b130150b90053
Bianca Baccay <bzbaccay@my.waketech.edu>
2025-01-29 12:59:32 -0500

    changed conditions, finsihed

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ mySubmit.onclick = function(){CheckAge()}
 function CheckAge(){
     age = myText.value;
     age = Number(age);
-    if(age == 0){
-        document.getElementById("resultAge").textContent = "Are you even old enough to talk?"
+    if(age < 0){
+        document.getElementById("resultAge").textContent = "Are you even alive?"
     }
     else if(age > 21){
         document.getElementById("resultAge").textContent = "Woah there, you're older than my older sister? Crazy."
     }
-    else if(age < 6){
+    else if(age == 5){
         document.getElementById("resultAge").textContent = "Go play on your iPad, kid."
     }
     else if(age >= 14){
